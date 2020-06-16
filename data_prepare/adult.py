07377aa8592ffd1eda0509566349245b827bdc47
Fabian <fab.beringer@gmail.com>
2020-06-16 19:02:53 +0200

    support generating multiple validation splits, add comments

--- data_prepare/adult.py
+++ data_prepare/adult.py
@@ -3,6 +3,7 @@ from urllib import parse, request
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 from data_prepare.base_prepare import DatasetPrepare
 
@@ -10,7 +11,16 @@ from data_prepare.base_prepare import DatasetPrepare
 class AdultPrepare(DatasetPrepare):
     """Downloads and pre-processes the Adult dataset."""
 
-    def __init__(self, target_dir, download_dir, base_url=None, n_buckets=5):
+    def __init__(self, target_dir, download_dir, base_url=None, n_buckets=5, n_splits=5, val_split=0.3):
+        """
+
+        :param target_dir: directory to put processed data
+        :param download_dir: directory for storing the downloaded raw data
+        :param base_url: base url to download the adult data files from
+        :param n_buckets: number of buckets for binary quantization of continuous features
+        :param n_splits: number of different train - validation splits to generate
+        :param val_split: percentage size of the validation split between (0, 1)
+        """
         super().__init__(target_dir, download_dir)
 
         default_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/adult/'
@@ -18,6 +28,8 @@ class AdultPrepare(DatasetPrepare):
         self.file_names = ['adult.' + name for name in ['data', 'test', 'names']]
 
         self.n_buckets = n_buckets
+        self.n_splits = n_splits
+        self.val_split = val_split
         self.col_names = ['age',
                           'workclass',
                           'fnlwgt',
@@ -82,11 +94,20 @@ class AdultPrepare(DatasetPrepare):
         x_onehot['label'] = y
         onehot_train, onehot_test = x_onehot[:train_len], x_onehot[train_len:]
 
-        # save to disk
-        train_file = os.path.join(self.target_dir, 'adult_train.pkl')
-        test_file = os.path.join(self.target_dir, 'adult_test.pkl')
+        # following Zemel et al. (2013) as suggested in the paper, we generate and store multiple validation splits
+        for i in range(self.n_splits):
+            train_split, val_split = train_test_split(onehot_train, test_size=self.val_split)
 
-        onehot_train.to_pickle(train_file)
+            # save to disk
+            split_dir = f'split_{i}'
+            os.makedirs(os.path.join(self.target_dir, split_dir), exist_ok=True)
+            train_file = os.path.join(self.target_dir, split_dir, 'adult_train.pkl')
+            val_file = os.path.join(self.target_dir, split_dir, 'adult_validation.pkl')
+
+            train_split.to_pickle(train_file)
+            val_split.to_pickle(val_file)
+
+        test_file = os.path.join(self.target_dir, 'adult_test.pkl')
         onehot_test.to_pickle(test_file)
 
     @staticmethod
