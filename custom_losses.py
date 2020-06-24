4669f9a97a62baa2886346d0ce85901063cc74bb
Fabian <fab.beringer@gmail.com>
2020-06-24 12:15:39 +0200

    implement kl terms for loss

--- custom_losses.py
+++ custom_losses.py
@@ -14,12 +14,31 @@ class VFAELoss(Module):
         self.ce = CrossEntropyLoss()
 
     def forward(self, y_pred, y_true):
-        # TODO add KL terms
+        """
+
+        :param y_pred: dict containing the vfae outputs
+        :param y_true: dict of ground truth labels for x, s and y
+        :return: the loss value as Tensor
+        """
         x, s, y = y_true['x'], y_true['s'], y_true['y']
         x_s = torch.cat([x, s], dim=-1)
 
         supervised_loss = self.ce(y_pred['y_decoded'], y)
         reconstruction_loss = self.bce(y_pred['x_decoded'], x_s)
 
-        loss = reconstruction_loss + self.alpha * supervised_loss
+        kl_loss_z1 = self._kl_gaussian(y_pred['z1_enc_logvar'], y_pred['z1_enc_std'],
+                                       y_pred['z1_dec_logvar'], y_pred['z1_dec_std'])
+
+        zeros = torch.zeros_like(y_pred['z1_enc_logvar'])
+        kl_loss_z2 = self._kl_gaussian(y_pred['z2_enc_logvar'], y_pred['z2_enc_std'],
+                                       zeros, zeros)
+
+        loss = reconstruction_loss + kl_loss_z1 + kl_loss_z2 + self.alpha * supervised_loss
+        # TODO MMD penalty
         return loss
+
+    @staticmethod
+    def _kl_gaussian(logvar_a, std_a, logvar_b, std_b):
+        per_example_kl = logvar_b - logvar_a - 1 + (logvar_a.exp() + (std_a - std_b).square()) / logvar_b.exp()
+        kl = 0.5 * torch.sum(per_example_kl, dim=1)
+        return kl.mean()
