d4d5e12a830bfef3e962b70679273de3778cc2e0
Fabian <fab.beringer@gmail.com>
2020-07-02 01:55:27 +0200

    add settings for checkpointing

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import hydra
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from lightning_wrapper import LightningModel
 
@@ -12,8 +13,18 @@ def train(cfg: DictConfig):
     seed_everything(cfg.random_seed)
 
     model = LightningModel(cfg)
-    trainer = Trainer(max_epochs=cfg.training.epochs, gpus=cfg.gpus, deterministic=True)
+
+    train_cfg = cfg.training
+    checkpoint_callback = ModelCheckpoint(save_top_k=train_cfg.save_ckpts,
+                                          monitor=train_cfg.monitor,
+                                          mode=train_cfg.mode)
+
+    trainer = Trainer(max_epochs=train_cfg.epochs,
+                      gpus=cfg.gpus,
+                      deterministic=True,
+                      checkpoint_callback=checkpoint_callback)
     trainer.fit(model)
+    trainer.test()
 
 
 if __name__ == '__main__':
