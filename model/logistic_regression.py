ba4751f1cccc4f84742f79bf25141decab8d73d6
Fabian <fab.beringer@gmail.com>
2020-07-02 01:56:40 +0200

    implement logistic regression on the vfae's latent vectors

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -1,4 +1,7 @@
+from hydra.utils import to_absolute_path
 from torch.nn import Module, Linear
+
+from lightning_wrapper import LightningModel
 
 
 class LogisticRegression(Module):
@@ -15,3 +18,28 @@ class LogisticRegression(Module):
 
     def forward(self, inputs):
         return self.lin(inputs)
+
+
+class LogisticRegressionVFAE(Module):
+    """Perform logistic regression on the representations from a pre-trained vfae (with frozen parameters)."""
+
+    def __init__(self, vfae_ckpt, z_dim, out_features):
+        """
+
+        :param vfae_ckpt: path to the vfae checkpoint to use for getting the latent representations.
+        :param z_dim: latent dimension of the vfae
+        :param out_features: number of output features
+        """
+        super().__init__()
+        vfae_ckpt = to_absolute_path(vfae_ckpt)
+        # load and freeze the vfae model
+        self.vfae = LightningModel.load_from_checkpoint(vfae_ckpt).model
+        for param in self.vfae.parameters():
+            param.requires_grad = False
+
+        self.log_regression = LogisticRegression(z_dim, out_features)
+
+    def forward(self, inputs):
+        x = self.vfae(inputs)['z1_encoded'].detach()
+        x = self.log_regression(x)
+        return x
